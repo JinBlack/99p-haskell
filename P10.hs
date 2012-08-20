cb7b7e2575f9eb448edfe203ade37a590cee758b
JinBlack <jinblack17@gmail.com>
2012-08-20 15:17:26 +0200

    ... and composition. High-Order Function Rulez

--- P10.hs
+++ P10.hs
@@ -1,5 +1,5 @@
 encode :: (Eq a) => [a] -> [(Int,a)]
-encode x = map (\x -> (length x, head x)) (foldr pack [] x)
+encode = map (\x -> (length x, head x)) . (foldr pack [])
 	where	pack x [] = [[x]]
 		pack x (y:z) = if x== (head y) then (x:y):z else [x]:y:z 
 
