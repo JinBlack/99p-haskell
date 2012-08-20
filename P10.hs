f517fe1a65c1bc7543a59c895bc231ad9ce330ed
JinBlack <jinblack17@gmail.com>
2012-08-20 15:12:34 +0200

    P10 with map and lambda. :)

--- P10.hs
+++ P10.hs
@@ -1,6 +1,5 @@
 encode :: (Eq a) => [a] -> [(Int,a)]
-encode x = code (foldr pack [] x)
+encode x = map (\x -> (length x, head x)) (foldr pack [] x)
 	where	pack x [] = [[x]]
 		pack x (y:z) = if x== (head y) then (x:y):z else [x]:y:z 
-		code [x] = [(length x,head x)]
-		code (x:y) = (length x,head x):(code y)
+
