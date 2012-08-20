bd160f852cdc96ce26f5c3c04453f8edfbb61821
JinBlack <jinblack17@gmail.com>
2012-08-20 17:39:00 +0200

    17 new solution

--- P17.hs
+++ P17.hs
@@ -1,3 +1,8 @@
-split :: [a] -> Int -> [[a]]
-split x n	| n >= 0 =(take n x):[(drop n x)]
-		| n < 0 = split x ((length x)+n)
+split :: [a] -> Int -> ([a],[a])
+split x n	| n >= 0 = ((take n x),(drop n x))
+
+split' :: [a] -> Int -> ([a],[a])
+split' [] _ = ([],[])
+split' ax@(x:xs) n	| n > 0 = (x:ys,zs)
+			| n == 0 = (ax,[])
+		where	(ys,zs) = split xs (n-1)
